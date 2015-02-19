58426318cb10c06162ce2440aded703f8daa6138
Readone !! ErcOne <android.armv7a@gmail.com>
2015-02-20 06:06:34 +0700

    bliss

--- bliss.mk
+++ bliss.mk
@@ -15,20 +15,20 @@
 #
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/common.mk)
+$(call inherit-product, vendor/bliss/config/common.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := GT-N7000
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/n7000/full_n7000.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n7000
-PRODUCT_NAME := cm_n7000
+PRODUCT_NAME := bliss_n7000
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-N7000
 
